1dac745a6fd9da73b92d2b790ae6346e56945f5c
AhmedMahfouz <ahmelsamahy@gmail.com>
2021-10-28 11:20:44 +0200

    changed layout of buttons and page
    they way they are aligned in the page

--- Working files/index.php
+++ Working files/index.php
@@ -22,31 +22,43 @@
 
 <body>
 <main>
+<div class="container">
+  
 
-    <div class="px-4 py-5 my-5 text-center" id="Top">
-      <img class="d-block mx-auto mb-4" src="./Used images/Full.jpg" alt="" width="360" height="285">
-      <!-- <h1 class="display-5 fw-bold">Pied Piper</h1> -->
-      <div class="col-lg-6 mx-auto my-3">
-      <button type="button" class="btn btn-primary">Primary</button>
-      <button type="button" class="btn btn-primary">Primary</button>
-      <button type="button" class="btn btn-primary">Primary</button>
-      <button type="button" class="btn btn-primary">Primary</button>
-      <button type="button" class="btn btn-primary">Primary</button>
+    <div class="px-4 py-5 text-center" id="Top">
 
+    <div class="row">
+      <div id="numButtons" class="col">
+      <div class="col-lg-auto mx-auto">
+        <button id="numButtons" type="button" class="btn ">0</button>
+        <button id="numButtons" type="button" class="btn ">1</button>
+        <button id="numButtons" type="button" class="btn ">2</button>
+        <button id="numButtons" type="button" class="btn ">3</button>
+        <button id="numButtons" type="button" class="btn ">4</button>
+  
+        </div>
+        <br>
+        <div class="col-lg-auto mx-auto">
+        <button id="numButtons" type="button" class="btn ">5</button>
+        <button id="numButtons" type="button" class="btn ">6</button>
+        <button id="numButtons" type="button" class="btn ">7</button>
+        <button id="numButtons" type="button" class="btn ">8</button>
+        <button id="numButtons" type="button" class="btn ">9</button>
+  
+        </div>
       </div>
-      <br>
-      <div class="col-lg-6 mx-auto">
-      <button type="button" class="btn btn-primary">Primary</button>
-      <button type="button" class="btn btn-primary">Primary</button>
-      <button type="button" class="btn btn-primary">Primary</button>
-      <button type="button" class="btn btn-primary">Primary</button>
-      <button type="button" class="btn btn-primary">Primary</button>
-
-      </div>
+    <div class="col">
+    <img class="d-block mx-auto mb-4" src="./Used images/Full.jpg" alt="" width="400" height="500">
+    </div>
+    <div class="col-sm">
+      <!-- will have input text here -->
+    </div>
+  </div>
     </div>
 
-    <div class="b-example-divider"></div>
-  </main>
+    <!-- <div class="b-example-divider"></div> -->
+  </div>
+</main>
     
 </body>
 <footer><?php include_once PROTECTED_DIR.'footer.php'; ?></footer>
