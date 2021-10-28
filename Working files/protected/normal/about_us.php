0325082d15a17a813b81f91da894610b3ce060ee
AhmedMahfouz <ahmelsamahy@gmail.com>
2021-10-28 08:30:48 +0200

    made the basic of webpage
    with the index file and routing for other pages with possible count of page in it and about us page

---
+++ Working files/protected/normal/about_us.php
@@ -0,0 +1,239 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>About us</title>
+  <link rel="shortcut icon" href="./Used images/sml.jpg">
+
+  <!-- Bootstrap core CSS -->
+  <link href="./assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+
+  <!-- Custom styles for this template -->
+  <link href="About us.css" rel="stylesheet">
+
+<body>
+  <!-- <div class="container">
+
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark
+      text-decoration-none">
+        <svg class="bi me-2" width="40" height="32">
+          <use xlink:href="#bootstrap" />
+        </svg>
+        <span class="fs-4">Pied Piper</span>
+      </a>
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="./index.html" class="nav-link">Home</a></li>
+        <li class="nav-item"><a href="./index.html#Features" class="nav-link">Features</a></li>
+        <li class="nav-item"><a href="./index.html#Pricing" class="nav-link">Pricing</a></li>
+        <li class="nav-item"><a href="./index.html#Sign up" class="nav-link">Sign up</a></li>
+        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">About us</a>
+        </li>
+      </ul>
+    </header>
+  </div> -->
+  <main>
+
+    <div class="container" id="Top">
+      <h1 class="display-6 fw-bold text-center mb-2 mt-5">About us</h1>
+      <center>
+        <hr style="width:15%">
+      </center>
+      <div class="col-lg-9 mx-auto mb-5">
+        <p class="lead mb-4 text-center">Goal of the project is to deliver new idea in the
+          websites that can connect between fans and their favorite artists to keep up with
+          new interviews and exclusive behind the scenes with their latest videos.
+        </p>
+        <p class="lead mb-4 text-center">
+          that's why we came with the idea of <span class="fs-4">Pide Piper</span> which is one of a kind website
+          that will fullfil your fan stamina to the artist you desire, as we believe that
+          it will be only good if people like you worked on it to deliver the best
+          experience that you would truly enjoy it and put us in the seat of you so we
+          live every piece of the code written behind it.
+        </p>
+      </div>
+      <table class="table table-borderless " id="juniors_table">
+        <tr>
+          <td>
+            <table class="table table-borderless">
+              <th class="text-center"> <img src="./Used images/About us/Ahmed.jpg" class="rounded-circle"
+                  id="personal_image_juniors" alt="Ahmed">
+              </th>
+              <tr>
+                <td class="text-center">
+                  <h3>Ahmed Mahfouz</h3>
+                </td>
+              </tr>
+              <tr>
+                <td class="text-center">
+                  <h6>Frontend developer, Team leader</h5>
+                </td>
+              </tr>
+            </table>
+          </td>
+          <td>
+            <table class="table table-borderless">
+              <th class="text-center"> <img src="./Used images/About us/Oleg.jpg" class="rounded-circle"
+                  id="personal_image_juniors" alt="Oleg">
+              </th>
+              <tr>
+                <td class="text-center">
+                  <h3>Oleg Dragoi</h3>
+                </td>
+              </tr>
+              <tr>
+                <td class="text-center">
+                  <h6>Database developer</h5>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+      <!-- Seniors section -->
+      <table class="table table-borderless" id="seniors_table">
+        <tr>
+          <td>
+            <!--  1  -->
+            <table class="table table-borderless">
+              <th class="text-center"> <img src="./Used images/About us/Tobi.jpg" class="rounded-circle"
+                  id="personal_image_seniors" alt="Tobi">
+              </th>
+              <tr>
+                <td class="text-center">
+                  <h3>Csanádi Balázs Tóbiás</h3>
+                </td>
+              </tr>
+              <tr>
+                <td class="text-center">
+                  <h6>Senior Developer</h5>
+                </td>
+              </tr>
+            </table>
+          </td>
+          <td>
+            <!--  2  -->
+            <table class="table table-borderless">
+              <th class="text-center"> <img src="./Used images/About us/Csaba Fügedi.jpg" class="rounded-circle"
+                  id="personal_image_seniors" alt="Csaba">
+              </th>
+              <tr>
+                <td class="text-center">
+                  <h3>Fügedi Csaba</h3>
+                </td>
+              </tr>
+              <tr>
+                <td class="text-center">
+                  <h6>Senior Developer</h6>
+                </td>
+              </tr>
+            </table>
+          </td>
+          <td>
+            <!--  3  -->
+            <table class="table table-borderless">
+              <th class="text-center"> <img src="./Used images/About us/ohm.jpg" class="rounded-circle"
+                  id="personal_image_seniors" alt="Oleg">
+              </th>
+              <tr>
+                <td class="text-center">
+                  <h3>Meghwar Om Parkash</h3>
+                </td>
+              </tr>
+              <tr>
+                <td class="text-center">
+                  <h6>Senior Developer</h6>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <!--  4  -->
+            <table class="table table-borderless">
+              <th class="text-center"> <img src="./Used images/About us/Reka.jpg" class="rounded-circle"
+                  id="personal_image_seniors" alt="Rebeka">
+              </th>
+              <tr>
+                <td class="text-center">
+                  <h3>Kungl Rebeka</h3>
+                </td>
+              </tr>
+              <tr>
+                <td class="text-center">
+                  <h6>Senior Developer</h5>
+                </td>
+              </tr>
+            </table>
+          </td>
+          <td>
+            <!-- 5  -->
+            <table class="table table-borderless">
+              <th class="text-center"> <img src="./Used images/About us/Ádám Ligárt.jpg" class="rounded-circle"
+                  id="personal_image_seniors" alt="Oleg">
+              </th>
+              <tr>
+                <td class="text-center">
+                  <h3>Ádám Ligárt</h3>
+                </td>
+              </tr>
+              <tr>
+                <td class="text-center">
+                  <h6>Senior Developer</h6>
+                </td>
+              </tr>
+            </table>
+          </td>
+
+          <td>
+            <!-- 6  -->
+            <table class="table table-borderless">
+              <th class="text-center"> <img src="./Used images/About us/Levet.jpg" class="rounded-circle"
+                  id="personal_image_seniors" alt="Levet">
+              </th>
+              <tr>
+                <td class="text-center">
+                  <h3>Molnár Levente</h3>
+                </td>
+              </tr>
+              <tr>
+                <td class="text-center">
+                  <h6>Senior Developer</h6>
+                </td>
+              </tr>
+            </table>
+          </td>
+      </table>
+      <p class=" lead mb-4 text-center">
+        we would appreciate your feedback if you have any problem on Lorem_ipsum@gmail.com
+      </p>
+
+      <!-- ---------------------------------------------------------------------------- -->
+      <!-- <footer class="py-3 my-4">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+
+          <li class="nav-item"><a href="#Top" class="nav-link px-2
+          text-muted">Home</a></li>
+          <li class="nav-item"><a href="./index.html#Features" class="nav-link px-2
+          text-muted">Features</a></li>
+          <li class="nav-item"><a href="./index.html#Pricing" class="nav-link px-2
+          text-muted">Pricing</a></li>
+          <li class="nav-item"><a href="./index.html#Sign up" class="nav-link px-2 text-muted">Sign
+              Up</a></li>
+          <li class="nav-item"><a href="#Top" class="nav-link px-2
+          text-muted">About us</a></li>
+        </ul>
+        <p class="text-center text-muted">&copy; 2021 Pied Piper, Inc</p>
+      </footer> -->
+    </div>
+  </main>
+
+  <script src="./assets/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
---
+++ Working files/protected/routing.php
@@ -0,0 +1,41 @@
+<?php
+if (!array_key_exists('P', $_GET) || empty($_GET['P'])) {
+    $_GET['P'] = 'home';
+}
+
+switch($_GET['P']) {
+    case 'home':
+        require_once NORMAL_DIR.'home.php';
+    break;
+    
+    case 'about_us':
+        require_once NORMAL_DIR.'about_us.php';
+        break;
+
+    case 'articles':
+        require_once NORMAL_DIR.'articles.php';
+        break;
+
+    case 'article':
+        require_once NORMAL_DIR.'article.php';
+        break;
+
+    case 'Sign_in':
+        require_once USER_DIR.'Sign_in.php';
+        break;
+
+    // case 'logout':
+	// 	IsUserLoggedIn() ? UserLogout(): header('Location: index.php');
+	break;
+    case 'Sign_Up':
+        require_once NORMAL_DIR.'Sign_Up.php';
+        break;
+    case 'add_article':
+        require_once USER_DIR.'add_article.php';
+        break;
+
+
+    default:
+        require_once NORMAL_DIR.'home.php';
+        break;
+}
