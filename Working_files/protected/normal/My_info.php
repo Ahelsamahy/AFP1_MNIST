4a6733c60eb6f96cda93c82837f2c09d5a2cea17
AhmedMahfouz <ahmelsamahy@gmail.com>
2021-11-11 17:53:49 +0100

    started the my_info file
    which should have the entered data from the user and not to mess it with the others

--- Working_files/protected/normal/My_info.php
+++ Working_files/protected/normal/My_info.php
@@ -1,25 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>MNIST</title>
-    <link rel="shortcut icon" href="./Used images/sml.jpg">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Bootstrap core CSS -->
-    <link href="./assets/dist/css/bootstrap.min.css" rel="stylesheet">
-    
-  <!-- Custom styles for this template -->
-  <link href="<?=PUBLIC_DIR.'index.css'?>" rel="stylesheet">
-
-   <!--- Bootrstrap javascript --->
-  <script src="./assets/dist/js/bootstrap.bundle.min.js"></script>
-</head>
-
-<body>
-<?php require_once PROTECTED_DIR.'navbar.php'; ?>
-
 <main>
 
 <div class="container">
 
-
+<p>asdadas</p>
 </main>
