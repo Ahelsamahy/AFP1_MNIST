245e7a3a496da8f4ca61f8dffcfd9f5dbc5eebfc
AhmedMahfouz <ahmelsamahy@gmail.com>
2021-11-11 20:11:01 +0100

    started features.php file
    that will have most of the vision for the team and the key features for the project for what makes it special

---
+++ Working_files/protected/normal/Features.php
@@ -0,0 +1,42 @@
+<main>
+
+<div class="container">
+
+<h1 class="display-6 fw-bold text-center mb-2 mt-5">Why</h1>
+      <center>
+        <hr style="width:10%">
+      </center>
+      <div class="col-lg-9 mx-auto mb-5">
+        <p class="lead mb-4 text-center">As in nowadays, it is hard for data analysts to improve their data bout the image recognition as there is a gap between what the scientist want and what the user can deliver, tools that deliver the info play a big role as it is the connection between the user and the info in lab so they get the required data as needed 
+        </p>
+        <p class="lead mb-4 text-center">
+          That's why we came with the idea of <span class="fs-4">MNIST</span> which is one of a kind website
+          that will make the users help in making the data anaylsts improve their algorithems so it make a 
+          better future for all of us. We believe that through our software we can make the world a better place
+          for future generations and we tend to carry this more.
+        </p>
+      </div>
+<h1 class="display-6 fw-bold text-center mb-2 mt-5">Our vision</h1>
+      <center>
+        <hr style="width:17%">
+      </center>
+      <div class="col-lg-9 mx-auto mb-5">
+        <p class="lead mb-4 text-center">Reduce the unintentional gap so the user can input the data as long as they want in user friendly-simple interface that won't be a problem for them to add as much input as they want, also taking care of how to deliver a solid info for the backend as it is for the analysts to keep it simple without much mess in it.
+        </p>
+      </div>
+      <center>
+        <hr style="width:80%">
+      </center>
+      <div class="row mx-5 my-5" id="one_image">
+        <div class="col-md-4 how-img">
+         <img src="./Used images/Features/buttons_look.png" class="img-fluid" alt=""/>
+       </div>
+    <div class="col-md-6 mx-5">
+<h1 class="display-2 mb-3" >Simplicity</h1>
+ <p > As everything that gets high value, having simple user interface was base stone in making the interface 
+   so the users can have stress-free time during their survey.
+ </p>
+</div>
+</div>
+</div>
+</main>
