9954125d0e56f3097f1f8fcce498e109a0f108e3
Oleg <oleg.dragoi228@gmail.com>
2021-11-28 14:42:52 +0100

    Edited Functions.sql
    Implemented Submit functionality to our database

--- Database/Functions.sql
+++ Database/Functions.sql
@@ -8,3 +8,28 @@ BEGIN
     LIMIT 1;
 END; $$
 DELIMITER ;
+
+DELIMITER $$
+CREATE OR REPLACE PROCEDURE EstimateValue(IN p_image_id int)
+BEGIN
+	DECLARE v_estimated_value int;
+    SELECT `value` FROM `submissions`
+	WHERE `image_id` = p_image_id
+	GROUP BY `value`
+	ORDER BY count(`value`) DESC
+	LIMIT 1
+	INTO v_estimated_value;
+
+	UPDATE `mnist_images` 
+	SET `estimated_value` = v_estimated_value 
+	WHERE `id` = p_image_id;
+END; $$
+DELIMITER ;
+	
+DELIMITER $$
+CREATE OR REPLACE PROCEDURE Submit(IN p_image_id int, IN p_value int)
+BEGIN
+    INSERT INTO `submissions`(`image_id`, `value`) VALUES (p_image_id, p_value);
+	CALL EstimateValue(p_image_id);
+END; $$
+DELIMITER ;
