e55489460a50531bd852b1f350140120bff2086b
Oleg <oleg.dragoi228@gmail.com>
2021-11-30 22:11:26 +0100

    Changed GetRandomImage in Functions.sql
    Minor changes in the select statement.

--- Database/Functions.sql
+++ Database/Functions.sql
@@ -1,10 +1,10 @@
 DELIMITER $$
 CREATE OR REPLACE PROCEDURE GetRandomImage()
 BEGIN
-    SELECT mi.id, mi.location from mnist_images mi
+    SELECT mi.id, mi.location FROM mnist_images mi
     LEFT JOIN submissions s ON mi.id = s.image_id
     GROUP BY mi.id
-    ORDER BY COUNT(mi.id), RAND()
+    ORDER BY COUNT(s.image_id), RAND()
     LIMIT 1;
 END; $$
 DELIMITER ;
