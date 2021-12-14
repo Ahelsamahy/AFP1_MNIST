ee7094a3dc9b7ff89abb68f87d57be63fc44a651
AhmedMahfouz <ahmelsamahy@gmail.com>
2021-12-14 18:41:16 +0100

    finished for the submission
    the addition

--- Working_files/protected/normal/home.php
+++ Working_files/protected/normal/home.php
@@ -1,13 +1,13 @@
 <?php
-  if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['image_id'])  && isset($_POST['value'])) {
-    require_once SUBMISSION_MANAGER;
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['image_id'])  && isset($_POST['value'])) {
+  require_once SUBMISSION_MANAGER;
 
-    addSubmission($_POST['image_id'], $_POST['value']);
-  }
+  addSubmission($_POST['image_id'], $_POST['value']);
+}
 
 
-  require_once IMAGE_MANAGER;
-  $image = getRandomImage();
+require_once IMAGE_MANAGER;
+$image = getRandomImage();
 
 ?>
 <main>
@@ -25,29 +25,22 @@
       <div class="row">
         <div id="numButtons" class="col">
           <div class="col-lg-auto mx-auto">
-            <button id="numButtons" type="button" class="btn" onclick="setValues(<?=$image['id'] ?>, '0')">0</button>
-            <button id="numButtons" type="button" class="btn">1</button>
-            <button id="numButtons" type="button" class="btn">2</button>
-            <button id="numButtons" type="button" class="btn">3</button>
-            <button id="numButtons" type="button" class="btn">4</button>
+            <button id="numButtons" type="button" class="btn" onclick="setValues(<?= $image['id'] ?>, '0')">0</button>
+            <button id="numButtons" type="button" class="btn" onclick="setValues(<?= $image['id'] ?>, '1')">1</button>
+            <button id="numButtons" type="button" class="btn" onclick="setValues(<?= $image['id'] ?>, '2')">2</button>
+            <button id="numButtons" type="button" class="btn" onclick="setValues(<?= $image['id'] ?>, '3')">3</button>
+            <button id="numButtons" type="button" class="btn" onclick="setValues(<?= $image['id'] ?>, '4')">4</button>
 
           </div>
           <br>
           <div class="col-lg-auto mx-auto">
-            <button id="numButtons" type="button" class="btn">5</button>
-            <button id="numButtons" type="button" class="btn">6</button>
-            <button id="numButtons" type="button" class="btn">7</button>
-            <button id="numButtons" type="button" class="btn">8</button>
-            <button id="numButtons" type="button" class="btn">9</button>
+            <button id="numButtons" type="button" class="btn" onclick="setValues(<?= $image['id'] ?>, '5')">5</button>
+            <button id="numButtons" type="button" class="btn" onclick="setValues(<?= $image['id'] ?>, '6')">6</button>
+            <button id="numButtons" type="button" class="btn" onclick="setValues(<?= $image['id'] ?>, '7')">7</button>
+            <button id="numButtons" type="button" class="btn" onclick="setValues(<?= $image['id'] ?>, '8')">8</button>
+            <button id="numButtons" type="button" class="btn" onclick="setValues(<?= $image['id'] ?>, '9')">9</button>
 
           </div>
-        </div>
-        <div>
-          <form method="POST">
-          <input type="hidden" id="image_id" name="image_id" value="">
-          <button type="submit" id="img_value" name="value" value="" class="button">submit</button>
-          </form>
-         
         </div>
         <div class="col">
           <img class="d-block mx-auto" src="<?= ($image <> false) ? $image['location'] : './Used images/Error/Image_Not_Found.png' ?>" alt="" width="350" height="350" id="image-show">
@@ -56,8 +49,13 @@
           <!-- will have input text here -->
         </div>
       </div>
+      <div class="row">
+      <form method="POST">
+            <input type="hidden" id="image_id" name="image_id" value="">
+           <center> <button type="submit" id="img_value" name="value" value="" class="btn">submit</button></center>
+          </form>
+      </div>
     </div>
-
     <!-- <div class="b-example-divider"></div> -->
   </div>
 </main>
